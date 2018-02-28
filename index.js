d374faadb56530f07d6844ce935fe981e403aa30
Alexis Astolfi <alexis.astolfi@gmail.com>
2018-02-28 03:10:48 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -28,3 +28,8 @@ function appendKitten(name) {
   var moreKittens = [...kittens, name];
   return moreKittens;
 }
+
+function prependKitten(name) {
+  var lessKittens = [name, ...kittens];
+  return kittens;
+}
