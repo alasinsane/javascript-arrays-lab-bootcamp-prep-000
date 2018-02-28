dc808826db9f82690144f3494ba9b300989569d0
Alexis Astolfi <alexis.astolfi@gmail.com>
2018-02-28 03:10:06 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -23,3 +23,8 @@ function destructivelyRemoveFirstKitten() {
   kittens.shift();  
   return kittens;
 }
+
+function appendKitten(name) {
+  var moreKittens = [...kittens, name];
+  return moreKittens;
+}
